7ef963938d7f1bfbe022d63ee1a42cc451fb7b83
Rachel <radub33@ulaval.ca>
2023-05-24 11:55:43 -0400

    Import data

---
+++ scripts/02_analyze_temporal_networks.R
@@ -0,0 +1,13 @@
+library(readr)
+library(tidyverse)
+data_network <- read_csv("data/raw/Cleaned_Ressources_Anonymous.csv")
+data_network <- read.table("data/raw/Cleaned_Ressources_Anonymous.csv", sep = ",",
+                           header = TRUE, row.names = 1)
+View(data_network)
+
+
+data_network %>% 
+  rename("test" = "...1")
+
+
+data_network[,1]
